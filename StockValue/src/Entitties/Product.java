dede377fbe3f9cdae7b3bf986917d4c261008094
Matheus Henrique Oliveira Barbosa <80551998+Mbarbosa15@users.noreply.github.com>
2022-10-24 16:31:24 -0300

    Atualizando o código

--- StockValue/src/Entitties/Product.java
+++ StockValue/src/Entitties/Product.java
@@ -5,6 +5,29 @@ public class Product {
 	public double price;
 	public int quantity;
 	
+	public Product(String name, double price, int quantity) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;	
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	
 	public double totalValorEstoque() {
 		return price * quantity;
 	}
